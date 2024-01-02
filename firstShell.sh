b0b4884234589e2c23fb5e59ba5c422d25213b99
Deepakkkxd <deepakkumarranjan95229@gmail.com>
2024-01-02 21:16:11 +0530

    new commit

--- firstShell.sh
+++ firstShell.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-a=$1 
-b=$2
+# Author : Deepak Kumar Ranjan
+# Date : April 29th 2021
+#
+# This script is to track AWS resource usage
+#
+# Things we will list
+# AWS EC2 Instances
+# AWS S3 Buckets
+# IAM Users
+#
+# Please note that this will give the output in JSON format and we will redirect only some specified information to the result file
+#
+#
+# AWS S3 Bucket
+echo "Printing list of S3 buckets"
+aws s3 ls
 
-sum=$(( $a+$b ))
+# AWS EC2 Instances
+echo "Printing list of EC2 Instances(Instance ID)"
+aws ec2 descirbe-instances | jq '.Reservations[].Instances[].InstanceId'
 
-echo "$sum" > result
+#AWS IAM Users
+echo "Printing list IAM users"
+aws iam list-users
